769b0c08bf60c47857fb4faf140d148cf7dac0ff
mm-tvkl <mohammadmahdi.tavakoli.1384@gmail.com>
2022-03-18 16:44:16 +0330

    Navbar and Dropdown added to Project

---
+++ Js/script.js
@@ -0,0 +1,22 @@
+let dropMenu2 = document.getElementById("dropMenu2");
+let navItemDrop2 = document.getElementById("navItemDrop2");
+
+dropMenu2.addEventListener("mouseenter" , function(){
+    navItemDrop2.style.backgroundColor = "#3b1d82";
+    navItemDrop2.style.color = "white";
+})
+
+dropMenu2.addEventListener("mouseleave" , function(){
+    navItemDrop2.style.backgroundColor = "white";
+    navItemDrop2.style.color = "#3b1d82";
+})
+
+navItemDrop2.addEventListener("mouseenter" , function(){
+    navItemDrop2.style.backgroundColor = "#3b1d82";
+    navItemDrop2.style.color = "white";
+})
+
+navItemDrop2.addEventListener("mouseleave" , function(){
+    navItemDrop2.style.backgroundColor = "white";
+    navItemDrop2.style.color = "#3b1d82";
+})
